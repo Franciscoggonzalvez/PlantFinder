e5f01ac4fba36f867ea36ad0e31feef4a03ce7c9
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-08-13 11:04:22 +0200

    Updates

---
+++ precache-manifest.94773bbd566abb8a7146421908ac3b71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8236b0b74088b47e0c72f2010735ab12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7eea2b28a00e66f288",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "314c5baef634f452fed2",
+    "url": "/static/css/main.46e6ce51.chunk.css"
+  },
+  {
+    "revision": "9b7eea2b28a00e66f288",
+    "url": "/static/js/2.6cc64ad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cc64ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314c5baef634f452fed2",
+    "url": "/static/js/main.9358501a.chunk.js"
+  },
+  {
+    "revision": "3e8da15a991f7476c76c",
+    "url": "/static/js/runtime-main.f3d3c7b6.js"
+  },
+  {
+    "revision": "0232458a51c0270e62b0a42b97fd7e64",
+    "url": "/static/media/BackgroundPlant.0232458a.jpg"
+  }
+]);
